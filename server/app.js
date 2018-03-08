e051573cc3eb4976cb98349f1bbcc19e6ccc5bcf
Betty Chow <bettychchow@gmail.com>
2018-03-08 15:23:40 -0800

    finished server side

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8000
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.disable('x-powered-by')
+
+if(process.env.NODE_ENV === 'development' ) {
+  app.use(morgan('dev'))
+}
+
+app.use(bodyParser.json())
+app.use(cors())
+
+
+const postsRoutes = require('./src/routes/posts')
+app.use('/posts', postsRoutes)
+
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  res.status(status).json({ error : { status, message: 'Someting went wrong'}})
+})
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: { message: 'Not found'}})
+})
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`)
+})
+
+module.exports = app
---
+++ server/src/models/posts.js
@@ -0,0 +1,112 @@
+const fs = require('fs')
+const path = require('path')
+const postsPath = path.join(__dirname, 'posts.json')
+const uuid = require('uuid/v4')
+
+const getAll = () => {
+  let posts = fs.readFileSync(postsPath, 'utf-8')
+  return posts
+}
+
+const getOne = (params) => {
+  let errors = []
+  let id = params.id
+  let posts = fs.readFileSync(postsPath, 'utf-8')
+  let postsArr = JSON.parse(posts)
+  let postFound = postsArr.find(post => post.id === id)
+  let response 
+
+  if(postFound) {
+    response = postFound
+  } else {
+    errors.push('Post not found')
+    response = { errors }
+  }
+  return response
+}
+
+const create = (body) => {
+  let errors = []
+  let title = body.title
+  let content = body.content
+  let posts = fs.readFileSync(postsPath, 'utf-8')
+  let postsArr = JSON.parse(posts)
+  let post = {}
+  let response
+
+  if(title && content) {
+
+    post = {
+      id: uuid(),
+      title,
+      content
+    }
+
+    postsArr.push(post)
+
+    let updatedPosts = JSON.stringify(postsArr)
+    fs.writeFileSync(postsPath, updatedPosts)
+    response = post
+  } else {
+    errors.push('Both title and content are required')
+    response = { errors }
+  }
+  return response
+}
+
+const update = (params, body) => {
+  let errors = []
+  let id = params.id
+  let title = body.title
+  let content = body.content
+  let posts = fs.readFileSync(postsPath, 'utf-8')
+  let postsArr = JSON.parse(posts)
+  let response
+
+  let postFound = postsArr.find(post => post.id === id)
+
+  if(!postFound) {
+      errors.push('Post not found')
+      response = errors
+    } else if (postFound && title && content) {
+      postsArr.forEach(post => {
+        if(post.id === id) {
+          post.title = title
+          post.content = content
+          response = post
+        }
+      })
+    } else {
+      errors.push('All fields required')
+      response = { errors }
+    }
+
+  let updatedPosts = JSON.stringify(postsArr)
+  fs.writeFileSync(postsPath, updatedPosts)
+  return response
+}
+
+const del = (params) => {
+  let errors = []
+  let id = params.id
+  let posts = fs.readFileSync(postsPath, 'utf-8')
+  let postsArr = JSON.parse(posts)
+  let response
+  let postFound = postsArr.find(post => post.id === id)
+  let index = postsArr.indexOf(postFound)
+console.log('ppppfffff', postFound)
+  if(postFound) {
+    response = postFound
+    postsArr.splice(index, 1)
+    let updatedPosts = JSON.stringify(postsArr)
+    fs.writeFileSync(postsPath, updatedPosts)
+  } else {
+    errors.push('Post not found')
+    console.log('eeeee', errors)
+    response = { errors }
+  }
+  console.log('rrreeeeesssss', response)
+  return response
+}
+
+module.exports = { getAll, getOne, create, update, del }
