b72823f3e7e3c1d8dbf42e8c458d7475d783c15e
Betty Chow <bettychchow@gmail.com>
2018-03-11 14:30:34 -0700

    moved app.is

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
-const postsRoutes = require('./src/routes/posts')
+const postsRoutes = require('./server/src/routes/posts')
 app.use('/posts', postsRoutes)
 
 
